85ee9298efe472551c125a1669d97e9de1ddcbf1
mcbarreraNC <mbarrera@uncc.edu>
2021-03-24 20:41:23 -0400

    tell me if you get this alex

--- Plots/heat.py
+++ Plots/heat.py
@@ -3,18 +3,19 @@ import plotly.graph_objs as go
 import pandas as pd
 
 # Load CSV file from Datasets folder
-df = pd.read_csv('../Datasets/CoronaTimeSeries.csv')
+df = pd.read_csv('../Datasets/Weather2014-15.csv')
 
 # Preparing data
-data = [go.Heatmap(x=df['Day'],
-                   y=df['WeekofMonth'],
-                   z=df['Recovered'].values.tolist(),
+data = [go.Heatmap(x=df['day'],
+                   y=df['month'],
+                   z=df['actual_max_temp'].values.tolist(),
                    colorscale='Jet')]
 
 # Preparing layout
-layout = go.Layout(title='Corona Virus Recovered Cases', xaxis_title="Day of Week",
-                   yaxis_title="Week of Month")
+layout = go.Layout(title='Max Temeperate on days of Week in Month', xaxis_title="Day of Week",
+                   yaxis_title="Month")
 
 # Plot the figure and saving in a html file
 fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig, filename='heatmap.html')
+pyo.plot(fig, filename='weatherheatmap.html')
+
